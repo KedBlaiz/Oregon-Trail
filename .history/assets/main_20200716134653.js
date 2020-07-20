b23ed04797acb35bf3ee8cbbd23def0f8271ac09
Kedersen <62950601+KedBlaiz@users.noreply.github.com>
2020-07-20 09:25:44 -0400

    .history

---
+++ .history/assets/main_20200716134653.js
@@ -0,0 +1,52 @@
+class Traveler {
+    constructor(name, food) {
+        this.name = name
+        this.food = 1
+        this.isHealthy = ('true')
+    }
+    hunt() {
+        this.food += 2
+    }
+    eat() {
+        if (this.food >0) {
+            this.food -= 1
+         
+        }
+        else {
+            return ('false')
+
+        }
+
+    }
+}
+
+class Wagon {
+    constructor(capacity, passengers) {
+        this.capacity = capacity
+        this.passengers = []
+
+    }
+    getAvailableSeatCount() {
+        const AvailableSeat = this.capacity
+        this.capacity - this.passengers.length
+        return ('AvailableSeat')
+    }
+    join(Traveler) {
+        if (this.getAvailableSeatCount === 0) {
+            this.passengers.push(travelers)
+        }
+
+    }
+    shouldQuarantine() {
+        const Quarantine = this.passengers.some(function (passengers) {
+         return passengers.isHealthy === false
+        })
+        return ('Quarantine')
+    }
+
+    totalFood() {
+        const totalFood = this.passengers.reduce(function(sum.))
+       
+        return ('totalFood')
+    }
+}
